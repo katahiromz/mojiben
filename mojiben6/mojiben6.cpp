bee3aabf54df5495ea6e0020a856b56874860079
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-08-02 18:18:43 +0900

    YUDANERU kakijun

--- mojiben6/mojiben6.cpp
+++ mojiben6/mojiben6.cpp
@@ -451,7 +451,7 @@ static const LPCWSTR g_aszMojiEnglish[200] =
     L"病:Disease, Illness, Sick",
     L"品:Goods, Product",
     L"負:Negative, Minus, Lose, Bear",
-    L"部:Part, Department, Category",
+    L"部:Part, Department, Category, Club",
     L"服:Clothes",
     L"福:Good Fortune",
     L"物:Object, Thing, Stuff",
