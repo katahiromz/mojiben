716a2a6f54de366829b25e9dfea56af643c2da31
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-10-30 17:55:55 +0900

    add compress.sh

---
+++ compress.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+rm -fr *part*.zip
+zip -9 -r mojiben-3.8-part1.zip mojiben-3.8-part1
+zip -9 -r mojiben-3.8-part2.zip mojiben-3.8-part2
+zip -9 -r mojiben-3.8-part3.zip mojiben-3.8-part3
+zip -9 -r mojiben-3.8-part4.zip mojiben-3.8-part4
+zip -9 -r mojiben-3.8-part5.zip mojiben-3.8-part5
+zip -9 -r mojiben-3.8-part6.zip mojiben-3.8-part6
+zip -9 -r mojiben-3.8-part7.zip mojiben-3.8-part7
+zip -9 -r mojiben-3.8-part8.zip mojiben-3.8-part8
+zip -9 -r mojiben-3.8-part9.zip mojiben-3.8-part9
