edcb1585a69e6966b618f58d2bfbc31875f79aff
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-11-04 11:22:46 +0900

    add mstr.h

---
+++ mstr.h
@@ -0,0 +1,140 @@
+// mstr.h - string manipulation
+#pragma once
+
+#include <string>
+#include <cctype>
+
+template <typename T_CHAR>
+inline void mstr_trim(std::basic_string<T_CHAR>& str, const T_CHAR *spaces) {
+    typedef std::basic_string<T_CHAR> string_type;
+    size_t i = str.find_first_not_of(spaces);
+    size_t j = str.find_last_not_of(spaces);
+    if ((i == string_type::npos) || (j == string_type::npos)) {
+        str.clear();
+    } else {
+        str = str.substr(i, j - i + 1);
+    }
+}
+
+template <typename T_CHAR>
+inline void mstr_trim_left(std::basic_string<T_CHAR>& str, const T_CHAR *spaces) {
+    typedef std::basic_string<T_CHAR> string_type;
+    size_t i = str.find_first_not_of(spaces);
+    if (i == string_type::npos) {
+        str.clear();
+    } else {
+        str = str.substr(i);
+    }
+}
+
+template <typename T_CHAR>
+inline void mstr_trim_right(std::basic_string<T_CHAR>& str, const T_CHAR *spaces) {
+    typedef std::basic_string<T_CHAR> string_type;
+    size_t j = str.find_last_not_of(spaces);
+    if (j == string_type::npos) {
+        str.clear();
+    } else {
+        str = str.substr(0, j + 1);
+    }
+}
+
+template <typename T_STR>
+inline bool
+mstr_replace(T_STR& str, const T_STR& from, const T_STR& to) {
+    bool ret = false;
+    size_t i = 0;
+    for (;;) {
+        i = str.find(from, i);
+        if (i == T_STR::npos)
+            break;
+        ret = true;
+        str.replace(i, from.size(), to);
+        i += to.size();
+    }
+    return ret;
+}
+
+template <typename T_STR>
+inline bool
+mstr_replace(T_STR& str, const typename T_STR::value_type *from, const typename T_STR::value_type *to) {
+    return mstr_replace(str, T_STR(from), T_STR(to));
+}
+
+template <typename T_STR_CONTAINER>
+inline void
+mstr_split(T_STR_CONTAINER& container,
+           const typename T_STR_CONTAINER::value_type& str,
+           const typename T_STR_CONTAINER::value_type& chars)
+{
+    container.clear();
+    size_t i = 0, k = str.find_first_of(chars);
+    while (k != T_STR_CONTAINER::value_type::npos) {
+        container.push_back(str.substr(i, k - i));
+        i = k + 1;
+        k = str.find_first_of(chars, i);
+    }
+    container.push_back(str.substr(i));
+}
+
+std::wstring mstr_unescape(const std::wstring& str) {
+    std::wstring ret;
+    ret.reserve(str.size());
+
+    for (size_t ich = 0; ich < str.size(); ++ich) {
+        if (str[ich] == L'\\' && ich + 1 < str.size()) {
+            ++ich;
+            switch (str[ich]) {
+            case L'\'': case L'\"': case L'?': case L'\\':
+                ret += str[ich];
+                break;
+
+            case L'a': ret += L'\a'; break;
+            case L'b': ret += L'\b'; break;
+            case L'f': ret += L'\f'; break;
+            case L'n': ret += L'\n'; break;
+            case L'r': ret += L'\r'; break;
+            case L't': ret += L'\t'; break;
+            case L'v': ret += L'\v'; break;
+
+            case L'x': { // hex (\xNN)
+                size_t start = ich + 1, len = 0;
+                while (start + len < str.size() && iswxdigit(str[start + len]))
+                    ++len;
+                if (len > 0) {
+                    int value = std::wcstol(str.substr(start, len).c_str(), NULL, 16);
+                    ret += static_cast<wchar_t>(value);
+                    ich += len;
+                }
+                break;
+            }
+
+            case L'u': { // Unicode (\uNNNN)
+                if (ich + 4 < str.size()) {
+                    int value = std::wcstol(str.substr(ich + 1, 4).c_str(), NULL, 16);
+                    ret += static_cast<wchar_t>(value);
+                    ich += 4;
+                }
+                break;
+            }
+
+            default:
+                ret += L'\\';
+                ret += str[ich];
+                break;
+            }
+        } else {
+            ret += str[ich];
+        }
+    }
+
+    return ret;
+}
+
+std::wstring mstr_unquote(const std::wstring& str) {
+    if (str.size() >= 2 && str.front() == L'"') {
+        if (str.back() == L'"')
+            return mstr_unescape(str.substr(1, str.size() - 2));
+        return mstr_unescape(str.substr(1, str.size() - 1));
+    }
+    return str;
+}
